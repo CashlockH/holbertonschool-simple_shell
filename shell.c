80d63f9439c6275283b8488ae520dea806df8206
CashlockH <ilyashesenli48@gmail.com>
2024-05-08 14:01:04 +0400

    getline checking

--- shell.c
+++ shell.c
@@ -1,25 +1,15 @@
 #include "main.h"
 int main (int ac, char ** av, char** env)
 {
-        char *buffer;
-        size_t bufsize = 5;
         char *args[] = { "bin", NULL, NULL};
-        buffer = malloc(bufsize*sizeof(char));
         if(isatty(0) && ac > 0)
         {
-               	printf("interactive mode\n");
+                printf("interactive mode\n");
         }
         else
         {
-                if (getline(&buffer,&bufsize,stdin) == -1)
-                        free(buffer);
-                if (buffer[strlen(buffer) - 1] == '\n')
-                        buffer[strlen(buffer) - 1] = '\0';
-                args[2] = buffer;
                 if (execve("/bin/ls", args, env) == -1)
                         perror(av[0]);
         }
         return 0;
 }
-
-
