9003a9041eb650127818ac5e2331cf9ce04b010e
CashlockH <ilyashesenli48@gmail.com>
2024-05-08 14:17:40 +0400

    execve checking

--- shell.c
+++ shell.c
@@ -1,5 +1,5 @@
 #include "main.h"
-int main (int ac, char ** av, char** env)
+int main (int ac, char ** av)
 {
         if(isatty(0) && ac > 0)
         {
@@ -7,7 +7,7 @@ int main (int ac, char ** av, char** env)
         }
         else
         {
-                execve("/bin/ls", av, env);
+                execve("/bin/ls", av, NULL);
         }
         return 0;
 }
