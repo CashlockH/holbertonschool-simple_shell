a7889d44172215d60e677870f405a6c2cd5ece44
CashlockH <ilyashesenli48@gmail.com>
2024-05-08 18:39:40 +0400

    execve checking

--- shell.c
+++ shell.c
@@ -3,14 +3,15 @@
 extern char** environ;
 int main(void)
 {
-	char *argv[] = {"/bin/ls", "-l", 0};
+	char *args[] = { "bin", NULL, NULL};
 	if (isatty(STDIN_FILENO)) 
 	{
 		fprintf(stderr, "Connected to a terminal");
     	} 
     	else 
 	{
-	        execve(argv[0], &argv[0], environ);
+		
+	        execve("/bin/ls", args, environ);
                 fprintf(stderr, "Oops!\n");
                 return -1;
 	}
