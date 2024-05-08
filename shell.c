d716de59a9c0b95cf935634580309e8ac98505df
CashlockH <ilyashesenli48@gmail.com>
2024-05-08 11:39:37 +0400

    simple shell

--- shell.c
+++ shell.c
@@ -1,20 +1,41 @@
 #include "main.h"
-int main ()
+int main (int ac, char **av)
 {
         char *buffer;
         size_t bufsize = 5;
-        int nese;
+        pid_t my_pid;
+        int blabla;
+        char *args[] = { "bin", NULL, NULL};
         buffer = malloc(bufsize*sizeof(char));
-        if(isatty(0))
+        if(isatty(0) && ac > 0)
         {
-                nese = getline(&buffer, &bufsize, stdin);
-                printf("%d", nese);
+                while (1)
+                {
+                        printf("#cisfun$ ");
+                        if (getline(&buffer,&bufsize,stdin) == -1)
+                                free(buffer);
+                        if (buffer[strlen(buffer) - 1] == '\n')
+                                buffer[strlen(buffer) - 1] = '\0';
+                        args[2] = buffer;
+                        my_pid = fork();
+                        if (my_pid != 0)
+                                wait(&blabla);
+                        if (my_pid == 0)
+                        {
+                                if (execve(buffer, args, NULL) == -1)
+                                        perror(av[0]);
+                        }
+                }
         }
         else
         {
                 if (getline(&buffer,&bufsize,stdin) == -1)
                         free(buffer);
-                printf("asdf");
+                if (buffer[strlen(buffer) - 1] == '\n')
+                        buffer[strlen(buffer) - 1] = '\0';
+                args[2] = buffer;
+                if (execve(buffer, args, NULL) == -1)
+                        perror(av[0]);
         }
         return 0;
 }
