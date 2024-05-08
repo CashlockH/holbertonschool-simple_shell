34d53c436d726a1803892e2dce353c02a862294b
CashlockH <ilyashesenli48@gmail.com>
2024-05-08 19:14:33 +0400

    checking

--- shell.c
+++ shell.c
@@ -1,27 +1,31 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 int main(void)
 {
-	char *argv[] = { "/bin/ls", 0};
-	char *envp[] = 
-	{
-		"HOME=/",
-		"PATH=/bin:/usr/bin",
-		"TZ=UTC0",
-		"USER=beelzebub",
-		"LOGNAME=tarzan",
-		0
-	};
-	if (isatty(STDIN_FILENO)) 
-	{
-		fprintf(stderr, "Connected to a terminal");
-    	} 
-    	else 
-	{
-		
-	        execve(argv[0], &argv[0], envp);
-                fprintf(stderr, "Oops!\n");
-                return -1;
-	}
-	return 0;
+    char *argv[] = { "/bin/ls", NULL };
+    char *envp[] =
+    {
+        "HOME=/",
+        "PATH=/bin:/usr/bin",
+        "TZ=UTC0",
+        "USER=beelzebub",
+        "LOGNAME=tarzan",
+        NULL
+    };
+
+    if (isatty(STDIN_FILENO))
+    {
+        fprintf(stderr, "Connected to a terminal\n");
+    }
+    else
+    {
+        execve(argv[0], argv, envp);
+        perror("execve");
+        return -1;
+    }
+
+    return 0;
 }
+
