51cad7fe64f10252f41229a43b89591996064e7f
CashlockH <ilyashesenli48@gmail.com>
2024-05-08 14:10:04 +0400

    execv checking

--- shell.c
+++ shell.c
@@ -1,14 +1,13 @@
 #include "main.h"
 int main (int ac, char ** av, char** env)
 {
-        char *args[] = { "bin", NULL, NULL};
         if(isatty(0) && ac > 0)
         {
                 printf("interactive mode\n");
         }
         else
         {
-                if (execve("/bin/ls", args, env) == -1)
+                if (execve("/bin/ls", av, env) == -1)
                         perror(av[0]);
         }
         return 0;
