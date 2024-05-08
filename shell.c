9af7c53a9577cac0cd96e1e01fba47629d735dd5
CaslockH <ilyashesenli48@gmail.com>
2024-05-08 18:16:30 +0400

    execve correction

--- shell.c
+++ shell.c
@@ -1,27 +1,72 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
 
-int main() 
-{
-        char *argv[] = {"/bin/ls", "-l", 0};
-        char *envp[] = {
-            "HOME=/",
-            "PATH=/bin:/usr/bin",
-            "TZ=UTC0",
-            "USER=beelzebub",
-            "LOGNAME=tarzan",
-            0};
-    if (isatty(0))
-    {
-        printf("interactive mode\n");
-    } 
-    else
-    {
-        execve(argv[0], &argv[0], envp);
-        fprintf(stderr, "Oops!\n");
-        return -1;
+int main(int ac, char** av) {
+    char *buffer = NULL;
+    char *argv[] = {NULL, NULL};
+    char *envp[] = {
+        "HOME=/",
+        "PATH=/bin:/usr/bin",
+        "TZ=UTC0",
+        "USER=beelzebub",
+        "LOGNAME=tarzan",
+        NULL};
+    size_t bufsize = 32; 
+    pid_t my_pid;
+    int blabla;
+    buffer = (char*)malloc(bufsize*sizeof(char));
+    if (buffer == NULL) { 
+	perror("malloc");
+        exit(EXIT_FAILURE);
     }
+
+    if (isatty(STDIN_FILENO) && ac > 0) { 
+	    while (1) {
+            printf("#cisfun$ ");
+            if (getline(&buffer, &bufsize, stdin) == -1) {
+                free(buffer);
+                exit(EXIT_FAILURE); 
+	    }
+            if (buffer[strlen(buffer) - 1] == '\n') {
+                buffer[strlen(buffer) - 1] = '\0';
+            }
+            argv[0] = buffer;
+            my_pid = fork();
+            if (my_pid == -1) {
+                perror("fork");
+                exit(EXIT_FAILURE);
+            }
+            if (my_pid != 0) {
+                wait(&blabla);
+            }
+            if (my_pid == 0) {
+                if (execve(argv[0], argv, envp) == -1) { 
+                    perror(av[0]); 
+                    exit(EXIT_FAILURE);
+                }
+            }
+        }
+    } else {
+        if (getline(&buffer, &bufsize, stdin) == -1) {
+            free(buffer);
+            exit(EXIT_FAILURE);
+        }
+        if (buffer[strlen(buffer) - 1] == '\n') {
+            buffer[strlen(buffer) - 1] = '\0';
+        }
+        argv[0] = buffer;
+        printf("%s\n", argv[0]);
+	if (execve(argv[0], argv, envp) == -1)
+	{ 
+		perror(av[0]); 
+		exit(EXIT_FAILURE);
+        }
+    }
+    free(buffer); 
     return 0;
 }
 
