b34f798b5a3ea3730b706c692de048a913515a94
CashlockH <ilyashesenli48@gmail.com>
2024-05-08 19:30:15 +0400

    code correction

--- shell.c
+++ shell.c
@@ -1,21 +1,40 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 extern char** environ;
-int main(void)
+int main (int ac, char **av)
 {
-    char *argv[] = { "ls", NULL };
-    if (isatty(STDIN_FILENO))
-    {
-        fprintf(stderr, "Connected to a terminal\n");
-    }
-    else
-    {
-        execve(argv[0], argv, environ);
-        perror("execve");
-        return -1;
-    }
-
-    return 0;
+        char *buffer;
+        size_t bufsize = 5;
+        pid_t my_pid;
+        int blabla;
+        char *args[] = { "bin", NULL};
+        buffer = malloc(bufsize*sizeof(char));
+        if(isatty(0) && ac > 0)
+        {
+                while (1)
+                {
+                        printf("#cisfun$ ");
+                        if (getline(&buffer,&bufsize,stdin) == -1)
+                                free(buffer);
+                        if (buffer[strlen(buffer) - 1] == '\n')
+                                buffer[strlen(buffer) - 1] = '\0';
+                        my_pid = fork();
+                        if (my_pid != 0)
+                                wait(&blabla);
+                        if (my_pid == 0)
+                        {
+                                if (execve(buffer, args, NULL) == -1)
+                                        perror(av[0]);
+                        }
+                }
+        }
+        else
+        {
+                if (getline(&buffer,&bufsize,stdin) == -1)
+                        free(buffer);
+                if (buffer[strlen(buffer) - 1] == '\n')
+                        buffer[strlen(buffer) - 1] = '\0';
+                if (execve(buffer, args, environ) == -1)
+                        perror(av[0]);
+        }
+        return 0;
 }
-
