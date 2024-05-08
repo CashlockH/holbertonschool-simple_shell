2eb315765d32fd9626dca311d77a334161fe306f
CashlockH <ilyashesenli48@gmail.com>
2024-05-08 19:25:26 +0400

    checking

--- shell.c
+++ shell.c
@@ -1,27 +1,17 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+extern char** environ;
 int main(void)
 {
     char *argv[] = { "ls", NULL };
-    char *envp[] =
-    {
-        "HOME=/",
-        "PATH=/bin:/usr/bin",
-        "TZ=UTC0",
-        "USER=beelzebub",
-        "LOGNAME=tarzan",
-        NULL
-    };
-
     if (isatty(STDIN_FILENO))
     {
         fprintf(stderr, "Connected to a terminal\n");
     }
     else
     {
-        execve(argv[0], argv, envp);
+        execve(argv[0], argv, environ);
         perror("execve");
         return -1;
     }
