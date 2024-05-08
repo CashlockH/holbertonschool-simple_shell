e4fdc7d1d8b7748dd524890b85bbdeee32205839
CashlockH <ilyashesenli48@gmail.com>
2024-05-08 14:23:59 +0400

    execve checker

--- shell.c
+++ shell.c
@@ -1,13 +1,17 @@
 #include "main.h"
-int main (int ac, char ** av)
+extern char **environ; 
+
+int main(int ac, char **av)
 {
-        if(isatty(0) && ac > 0)
-        {
-                printf("interactive mode\n");
-        }
-        else
-        {
-                execve("/bin/ls", av, NULL);
-        }
-        return 0;
+    if (isatty(0) && ac > 0)
+    {
+        printf("interactive mode\n");
+    }
+    else
+    {
+        execve("/bin/ls", av, environ);
+        perror(av[0]); 
+    }
+    return 0;
 }
+
