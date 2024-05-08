e3cf6aef5181625759b7dadd7980d6a7e44367e9
CashlockH <ilyashesenli48@gmail.com>
2024-05-08 18:42:00 +0400

    execve checking

--- shell.c
+++ shell.c
@@ -11,7 +11,7 @@ int main(void)
     	else 
 	{
 		
-	        execve("/bin/ls", args, environ);
+	        execve("/bin/ls", &args[0], environ);
                 fprintf(stderr, "Oops!\n");
                 return -1;
 	}
