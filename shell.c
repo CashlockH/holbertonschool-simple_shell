1568aa0bb5e15f48404f348a493d5dfcb5ec30a9
CashlockH <ilyashesenli48@gmail.com>
2024-05-09 12:45:49 +0400

    return value checking

--- shell.c
+++ shell.c
@@ -1,64 +1,15 @@
 #include "main.h"
 extern char** environ;
-int main (int ac, char **av)
+int main (void)
 {
-        size_t bufsize = 5;
-        pid_t my_pid;
-        int status;
-        char *buffer = malloc(sizeof(char) * bufsize);
-        char *args[65];
-        if(isatty(0) && ac > 0)
+        if(isatty(0))
         {
-                while (1)
-                {
-                printf("#cisfun$ ");
-                        if (getline(&buffer,&bufsize,stdin) == -1)
-                        {
-                                free(buffer);
-                                perror("getline");
-                                exit(EXIT_FAILURE);
-                        }
-                        if (buffer[strlen(buffer) - 1] == '\n')
-                                buffer[strlen(buffer) - 1] = '\0';
-                        my_pid = fork();
-                        if (my_pid == -1) {
-                            perror("fork");
-                            exit(EXIT_FAILURE);
-                        }
-                        if (my_pid == 0)
-                        {
-                                if (execve(buffer, args, environ) == -1)
-                                {
-                                        perror(av[0]);
-                                        exit(EXIT_FAILURE);
-                                }
-                         
-                        }
-                        else
-                        {
-                                    waitpid(my_pid, &status, 0);
-                                    if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
-                                        exit(EXIT_SUCCESS);
-                        }
-                }
+                printf("asdfsdf");
         }
         else
         {
-                if (getline(&buffer,&bufsize,stdin) == -1)
-                {
-                        free(buffer);
-                        perror("getline");
-                        exit(EXIT_FAILURE);
-                }
-                if (buffer[strlen(buffer) - 1] == '\n')
-                        buffer[strlen(buffer) - 1] = '\0';
-                if (execve(buffer, args, environ) == -1)
-                {
-                        fprintf(stderr, "%s: 1: %s: not found\n", av[0], buffer);
-                        exit(EXIT_FAILURE);
-                }
+                printf("ashdfsfd");
         }
-        free(buffer);
-        return(status);
+        return(60);
 }
 
