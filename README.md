9a2416204217091b2f6407818b025712ce8ce39b
CashlockH <ilyashesenli48@gmail.com>
2024-05-12 23:49:50 +0400

    README.md is corrected

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-This will be the README of the task
+# Holberton School Simple Shell Project
+
+## SHELL
+This project represents a minimal shell implementation, acting as a command language interpreter. It reads commands from a command line string, operating in both interactive and non-interactive modes. It encompasses basic command-line interface functionalities such as command execution, management of environment variables, and display of environment variables. 
+
+## ABOUT
+
+The main function oversees the program flow, encompassing tasks such as reading user input, handling built-in commands (exit and env), creating child processes, and executing commands. It employs custom_getenv and args_writer functions for handling environment variable retrieval and executable path resolution, respectively.
+
+The custom_getenv function retrieves the value of a specified environment variable. It iterates through the environ array, extracting key-value pairs and storing the values in the provided args array.
+
+The _printenv function outputs the contents of environment variables to the console.
+
+The args_writer function locates the executable for a given command. It retrieves the PATH environment variable, iterates through its directories, and checks if the combined path is executable. Upon finding a valid executable, it stores the path in the provided argv array.
+
+The flowchart includes detailed explanations for each step, decision point, and function call, offering a comprehensive insight into the code's logic and structure.
+
+## FILES
+
+
+### [README.md](./README.md) 
+* Description of Simple Shell Repository
+### [AUTHORS](./AUTHORS) 
+* Contributors of Simple Shell Repository
+### [main.h](./main.h) 
+* Declarations and Prototypes for Simple Shell Program 
+### [shell.c](./shell.c) 
+* Main Program for Simple Shell
+### [man_1_simple_shell](./man_1_simple_shell) 
+* Manual Page of Simple Shell
+
+### Command Execution
+* Executes user-entered commands.
+
+### Environment Variable Handling
+* Offers functions for retrieving and displaying environment variables.
+
+### Built-in Commands
+* Includes built-in commands like exit for shell termination and env for displaying environment variables.
+
+## Features
+
+### Dynamic Path Resolution
+* Noteworthy feature allowing dynamic resolution of paths. When a command is entered without an absolute path, the shell searches through directories listed in the PATH environment variable to find the executable file, enabling seamless command execution without specifying full paths.
+
+### Error Handling
+* Robust error handling mechanisms are incorporated to gracefully manage various scenarios, such as command not found, memory allocation errors, and fork failures. Informative error messages assist users in understanding encountered issues.
+
+### Clean Input Parsing
+* Efficient and clean input parsing. User input is tokenized for separating commands and arguments, ensuring accurate execution and preventing common input-related errors.
+
+### Process Management
+* Effective management of child processes spawned for command execution. Utilizes fork system call for creating child processes, waits for their completion using wait system call, and ensures proper handling of process statuses and exit codes.
+
+
+## Flowchart
+
+<p align="center"><img src="./flowchart.png"></p>
+
+
+
+### Compilation
+
+```
+$ gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.c -o hsh
+
+```
+
+## Authors
+*	Ilyas Hasanli - [Github]https://github.com/CashlockH/
+* Ismayilov Farid - [Github]https://github.com/Fariidismail/
--- shell.c
+++ shell.c
@@ -1,9 +1,209 @@
+#include "main.h"
 /**
- * main - function
- * Return: 0
+ * custom_getenv - Gets the value of a specific environment variable.
+ * @name: The name of the environment variable to retrieve.
+ * @args: An array used to store the retrieved value, limited to a maximum size of 64 characters.
+ * Return: Returns either the value, 1 if there's an issue, or 0 if successful.
  */
-int main(void)
+int custom_getenv(const char* name, char *args[64])
 {
-	printf("Hello");
-	return (0);
+    extern char** environ;
+    int j = 0, a = 0;
+    size_t i;
+        char *env_var;
+        char *token, *zoken;
+    for (i = 0; environ[i] != NULL; i++)
+    {
+        env_var = strdup(environ[i]);
+        token = strtok(env_var, "=");
+        if (token != NULL && strcmp(token, name) == 0)
+        {
+              a = 0;   
+            token = strtok(NULL, "=");
+                if(token == NULL)
+                {
+                        free(env_var);
+                        a = 1;
+                        break;
+                }
+                zoken = strtok(token, ":");        
+            while (token != NULL && zoken != NULL)
+            {
+                args[j] = strdup(token), j++;
+                token = strtok(NULL, ":");
+            }
+            args[j] = NULL;
+                free(env_var);
+            break;
+        }
+        else
+                a = 1;
+        free(env_var);
+    }
+        return a;
+}
+
+/**
+ * _printenv - Displays the contents of environment variables.
+ * @envi: Pointer to an array of strings representing environment variables.
+ */
+void _printenv(char **envi)
+{
+        int i = 0;
+        while(envi[i] != NULL)
+                {
+                        printf("%s\n", envi[i]);
+                        i++;
+                }
+}
+
+
+/**
+ * args_writer - Writes the contents of an array of strings to a buffer.
+ * @arv: An array of strings to write to the buffer.
+ * @code_holder: Pointer to the buffer where the contents will be written.
+ * Return: Returns 1 upon completion.
+ */
+int args_writer(char *arv[64], char *code_holder)
+{
+    char *args[64];
+    char *nese = strdup(code_holder);
+    int i = 0, j = 0, geti = 0;
+        args[0] = NULL;
+        geti = custom_getenv("PATH", args);
+
+    while (args[i])
+    {        
+        strcat(args[i], "/");
+        strcat(args[i], nese);
+        if (access(args[i], X_OK) == 0)
+        {
+            arv[j] = strdup(args[i]);
+            break;
+            j++;
+        }
+        i++;
+    }
+    free(nese);
+    return geti;
+}
+
+
+/**
+ * main - Program entry point.
+ * @ac: Number of command-line arguments received.
+ * @av: Array of strings representing the command-line arguments.
+ * Return: Returns 0 or 1.
+ */
+int main(int ac, char **av)
+{
+        pid_t my_pid;
+        size_t bufsize = 64;
+        int status, status_tutan = 5, nese = 0;
+        char *args[64];
+        char *buffer = malloc(bufsize * sizeof(char));
+        char *token;
+        int i = 0, j, writer = 0;
+        if (!buffer)
+        {
+                perror("malloc");
+                exit(EXIT_FAILURE);
+        }
+
+        while (ac > 0 && nese != EOF)
+        {
+                nese = getline(&buffer, &bufsize, stdin);
+                if (nese == 1)
+                        continue;
+                else if (nese == -1)
+                        break;
+                else
+                {
+                        args[0] = NULL;
+                        args[1] = NULL;
+                        i = 0;
+                        if (buffer[strlen(buffer) - 1] == '\n')
+                                buffer[strlen(buffer) - 1] = '\0';
+                        token = strtok(buffer, " \n\t");
+                        if (token == NULL)
+                        {
+                                free(buffer);
+                                exit(EXIT_SUCCESS);
+                        }
+                        while (token != NULL)
+                        {
+                                args[i] = strdup(token);
+                                if (!args[i])
+                                {
+                                        perror("strdup");
+                                        exit(EXIT_FAILURE);
+                                }
+                                token = strtok(NULL, " \n\t");
+                                i++;
+                        }
+                        args[i] = NULL;
+                        if (strcmp(args[0], "exit") == 0)
+                        {
+                                free(buffer);
+                                for (j = 0; j < i; j++)
+                                        free(args[j]);
+                                if (status_tutan == 5)
+                                        exit(0);
+                                else if (status_tutan == 512)
+                                        exit(2);
+                                else
+                                        exit(EXIT_SUCCESS);
+                        }
+                        if (strcmp(args[0], "env") == 0)
+                        {        
+
+                                free(buffer);
+                                for(j = 0; j < i; j++)
+                                        free(args[j]);
+
+                                _printenv(environ);
+                                exit(EXIT_SUCCESS);
+
+                        }
+                        my_pid = fork();
+                        if (my_pid == -1)
+                        {
+                                perror("fork");
+                                exit(EXIT_FAILURE);
+                        }
+                        else if (my_pid == 0)
+                        {
+
+                                if (strchr(args[0], '/') == 0)
+                                {
+                                        writer = args_writer(args, args[0]);
+                                }
+                                if (writer == 1)
+                                {
+                                        fprintf(stderr, "%s: 1: %s: not found\n", av[0], buffer);
+                                        free(buffer);
+                                        for (j = 0; j < i; j++)
+                                        free(args[j]);
+                                        exit(127);
+                                }
+                                else if (execve(args[0], args, environ) == -1)
+                                {
+                                        fprintf(stderr, "%s: 1: %s: not found\n", av[0], buffer);
+                                        free(buffer);
+                                        for (j = 0; j < i; j++)
+                                        free(args[j]);
+                                        exit(127);
+                                }
+                        }
+                        else
+                        wait(&status);
+                        status_tutan = status;
+                        for (j = 0; j < i; j++)
+                        free(args[j]);
+                } 
+        }
+        free(buffer);
+        if (status_tutan == 32512)
+                return(127);
+        return (0);
 }
